4a50a5723be203e614f18dde1126152652f4e669
kirabian <fabiansyahalghiffarireal@gmail.com>
2025-12-24 15:39:35 +0700

    2

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,6 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Di sini tidak perlu pakai prefix /api lagi karena sudah otomatis
 Route::post('/v1/register', [AuthController::class, 'register']);
 Route::post('/v1/login', [AuthController::class, 'login']);
-
-Route::get('/', function () {
-    return view('welcome');
-});
