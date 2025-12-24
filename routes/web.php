f45272d5850fc9ce6faeaa31184c02862bd17d06
kirabian <fabiansyahalghiffarireal@gmail.com>
2025-12-24 15:42:53 +0700

    3

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Di sini tidak perlu pakai prefix /api lagi karena sudah otomatis
+// Di sini tidak perlu menulis '/api' karena Laravel otomatis menambahkannya
 Route::post('/v1/register', [AuthController::class, 'register']);
 Route::post('/v1/login', [AuthController::class, 'login']);
